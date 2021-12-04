b3f1d44ae64554544f041f946c5bed1ccaed1bf7
jushih <jvshih@gmail.com>
2021-12-03 22:19:05 -0800

    Prettify code

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,61 +1,54 @@
 function findAccountById(accounts, id) {
   const match = accounts.find((each) => each.id === id);
-  console.log(match)
-  return match
+  console.log(match);
+  return match;
 }
 
 function sortAccountsByLastName(accounts) {
-  return accounts.sort((a, b)  => (a.name.last > b.name.last )? 1:-1);
+  return accounts.sort((a, b) => (a.name.last > b.name.last ? 1 : -1));
 }
 
 function getTotalNumberOfBorrows(account, books) {
-  
-  let count = 0
-  books.forEach(book => {
+  let count = 0;
+  books.forEach((book) => {
     // for each book, get the array in borrows
     let { borrows } = book;
-    borrows.forEach(id => {
-      console.log(id.id,account.id)
-      if(id.id === account.id) {
+    borrows.forEach((id) => {
+      console.log(id.id, account.id);
+      if (id.id === account.id) {
         count++;
       }
-    })
-  })
-  return count
+    });
+  });
+  return count;
 }
-
 
 function findAuthorById(authors, id) {
   const match = authors.find((each) => each.id === id);
-  return match
+  return match;
 }
 
-
 function getBooksPossessedByAccount(account, books, authors) {
-
   let bookArray = [];
-  books.forEach(book => {
+  books.forEach((book) => {
     // for each book, get the array in borrows
     let { borrows } = book;
-    borrows.forEach(id => {
-     // check whether the book is borrowed by the account
-      if(id.id === account.id && id.returned === false) {
-        let author = findAuthorById(authors, book.authorId)
+    borrows.forEach((id) => {
+      // check whether the book is borrowed by the account
+      if (id.id === account.id && id.returned === false) {
+        let author = findAuthorById(authors, book.authorId);
         //book["author"] = author
         //console.log(book)
         //console.log('-----------')
-        const newBook = {...book, author:author}
-        console.log(newBook)
-        bookArray.push(newBook)
-        return bookArray
+        const newBook = { ...book, author: author };
+        console.log(newBook);
+        bookArray.push(newBook);
+        return bookArray;
       }
-    })
-  })
-  console.log(bookArray)
-  return bookArray
-
-  
-  
+    });
+  });
+  console.log(bookArray);
+  return bookArray;
 }
 
 module.exports = {
